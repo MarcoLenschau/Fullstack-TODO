9b2f2d85a6c3f328ebd7b3a20f2c6bcc8e7bad9f
MarcoLenschau <contact@marco-lenschau.de>
2025-10-15 04:14:22 +0200

    chore: Improve the task class with more informatics

--- backend/task.py
+++ backend/task.py
@@ -3,4 +3,5 @@ from config import db, datetime
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
-    created = db.Column(db.DateTime, default=datetime.now())
+    dueDate = db.Column(db.DateTime, default=datetime.now()) 
+    isDone = db.Column(db.Boolean, default=False) 
