5a01b22ce90e4749d903dbbc87e6deff0170e45e
MarcoLenschau <contact@marco-lenschau.de>
2025-10-22 18:16:01 +0200

    feat: Add swagger ui in the project with test data

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,24 @@
 from config import app, request
 from data import Data
+from flask import send_from_directory
+from flask_swagger_ui import get_swaggerui_blueprint
 
 data = Data()
+
+SWAGGER_URL = "/api"
+API_SPEC_PATH = "openapi.yaml"
+API_URL = f"/{API_SPEC_PATH}"
+
+swagger_bp = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={"app_name": "Fullstack TODO API"}
+)
+app.register_blueprint(swagger_bp, url_prefix=SWAGGER_URL)
+
+@app.route(API_URL)
+def swagger_spec():
+    return send_from_directory(".", API_SPEC_PATH, mimetype="application/yaml")
 
 @app.route("/tasks", methods=["GET", "POST"])
 def taskRoute():
