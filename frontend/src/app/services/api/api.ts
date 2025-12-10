871085ff7f67eceb1eb97732af9ea5cbf23dc3ee
MarcoLenschau <contact@marco-lenschau.de>
2025-12-10 01:18:11 +0100

    chore: Work on docker support at the backend

--- frontend/src/app/services/api/api.ts
+++ frontend/src/app/services/api/api.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class Api {
-  private apiUrl = "http://127.0.0.1:5000";
+  private apiUrl = "http://localhost:5000";
   private http = inject(HttpClient);
 
   /**
